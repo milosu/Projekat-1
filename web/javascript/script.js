571921e982d38cfe09d1657f9b7911fcb95b8d12
Milos Usljebrka <serbianplayer@live.com>
2019-03-06 11:01:08 +0100

    Delleted comment from script.js

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -1,9 +1,8 @@
-  $(function () {
+  git$(function () {
     $(window).on("scroll", function () {
       if ($(window).scrollTop() > 50) {
         $(".navbar").addClass("nav-active");
       } else {
-        //remove the background property so it comes transparent again (defined in your css)
         $(".navbar").removeClass("nav-active");
       }
     });
