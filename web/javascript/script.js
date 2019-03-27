52a9c88020eb28e2b0fa198ec53851c6d8fe2f89
Milos Usljebrka <serbianplayer@live.com>
2019-03-27 13:42:21 +0100

    added more padding on section #2 heading

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -7,3 +7,25 @@
       }
     });
   });
+
+
+  // scrole with animation
+
+$(document).ready(function () {
+  $('body').scrollspy({ target: ".navbar", offset: 50 });
+
+  $("#navbarNav a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        window.location.hash = hash;
+      });
+    }  
+  });
+});
